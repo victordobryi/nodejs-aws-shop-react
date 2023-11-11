fe34b0d30a4df239197a15ca3831dd0836e002f2
bazla <dobr1nya@mail.ru>
2023-11-11 14:04:36 +0300

    feat: add S3 Bucket creation and deployment in CloudFront with automatic cache update

--- react-app/src/components/MainLayout/MainLayout.tsx
+++ react-app/src/components/MainLayout/MainLayout.tsx
@@ -1,19 +1,19 @@
-import React from "react";
-import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
-import Container from "@mui/material/Container";
-import Header from "~/components/MainLayout/components/Header";
-import Box from "@mui/material/Box";
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import Container from '@mui/material/Container';
+import Header from '~/components/MainLayout/components/Header';
+import Box from '@mui/material/Box';
 
 function Copyright() {
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/" underline="hover">
-        My Store
-      </Link>{" "}
+    <Typography variant='body2' color='textSecondary' align='center'>
+      {'Copyright © '}
+      <Link color='inherit' href='https://material-ui.com/' underline='hover'>
+        Aws-React-Shop 3
+      </Link>{' '}
       {new Date().getFullYear()}
-      {"."}
+      {'.'}
     </Typography>
   );
 }
@@ -23,20 +23,15 @@ const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     <>
       <Header />
       <main>
-        <Container sx={{ pb: 8 }} maxWidth="md">
+        <Container sx={{ pb: 8 }} maxWidth='md'>
           {children}
         </Container>
       </main>
       <Box
-        component={"footer"}
+        component={'footer'}
         sx={{ bgcolor: (theme) => theme.palette.background.paper, padding: 6 }}
       >
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="textSecondary"
-          component="p"
-        >
+        <Typography variant='subtitle1' align='center' color='textSecondary' component='p'>
           Thank you for your purchase!
         </Typography>
         <Copyright />
