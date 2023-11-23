e007eb590585bb1fc35afa70eca9eb053b8e95db
bazla <dobr1nya@mail.ru>
2023-11-23 23:50:57 +0300

    refactor: fix api path

--- react-app/src/constants/apiPaths.ts
+++ react-app/src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
-  product: `https://0buwuddcde.execute-api.us-east-1.amazonaws.com/dev/products`,
+  product: `https://pajr83rnib.execute-api.us-east-1.amazonaws.com/prod/products/`,
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://0buwuddcde.execute-api.us-east-1.amazonaws.com/dev/products',
+  bff: 'https://pajr83rnib.execute-api.us-east-1.amazonaws.com/prod/products/',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
