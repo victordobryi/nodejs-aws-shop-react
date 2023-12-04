cc344275fea233eebec5ebd016ecc8d9a788b372
bazla <dobr1nya@mail.ru>
2023-12-04 12:25:38 +0300

    :feat: add upload file logic

--- react-app/src/constants/apiPaths.ts
+++ react-app/src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: `https://pajr83rnib.execute-api.us-east-1.amazonaws.com/prod/products/`,
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://v99eyvhbhl.execute-api.us-east-1.amazonaws.com/prod',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
