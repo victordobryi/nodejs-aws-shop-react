08abc69a6f2703ad72949e5d9fbc3f33d01887f3
bazla <dobr1nya@mail.ru>
2024-01-07 02:13:13 +0300

    refactor: change cart, bff links

--- react-app/src/constants/apiPaths.ts
+++ react-app/src/constants/apiPaths.ts
@@ -2,8 +2,8 @@ const API_PATHS = {
   product: `https://pajr83rnib.execute-api.us-east-1.amazonaws.com/prod/products/`,
   order: 'https://22290s6g91.execute-api.us-east-1.amazonaws.com/prod/api',
   import: 'https://v99eyvhbhl.execute-api.us-east-1.amazonaws.com/prod',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://victordobryi-cart-api-docker.us-east-1.elasticbeanstalk.com/',
+  bff: 'http://victordobryi-bff-api-bffdev.us-east-1.elasticbeanstalk.com',
+  cart: 'http://victordobryi-cart-api-cartdev.us-east-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
