fdcebf63dd37a4f4d2579af50d15955cf973ddd0
bazla <dobr1nya@mail.ru>
2024-01-07 02:43:15 +0300

    refactor: fix product, cart links

--- react-app/src/constants/apiPaths.ts
+++ react-app/src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: `https://pajr83rnib.execute-api.us-east-1.amazonaws.com/prod/products/`,
+  product: `http://victordobryi-bff-api-bffdev.us-east-1.elasticbeanstalk.com/product`,
   order: 'https://22290s6g91.execute-api.us-east-1.amazonaws.com/prod/api',
   import: 'https://v99eyvhbhl.execute-api.us-east-1.amazonaws.com/prod',
   bff: 'http://victordobryi-bff-api-bffdev.us-east-1.elasticbeanstalk.com',
-  cart: 'http://victordobryi-cart-api-cartdev.us-east-1.elasticbeanstalk.com/api',
+  cart: 'http://victordobryi-bff-api-bffdev.us-east-1.elasticbeanstalk.com/cart',
 };
 
 export default API_PATHS;
