b33761cf52e19b077744f9f2f79cf3d14cafa10a
bazla <dobr1nya@mail.ru>
2024-01-07 03:20:13 +0300

    feat: add https proxy to bff

--- react-app/src/constants/apiPaths.ts
+++ react-app/src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: `http://victordobryi-bff-api-bffdev.us-east-1.elasticbeanstalk.com/product`,
+  product: `https://ipb1k38853.execute-api.us-east-1.amazonaws.com/product`,
   order: 'https://22290s6g91.execute-api.us-east-1.amazonaws.com/prod/api',
   import: 'https://v99eyvhbhl.execute-api.us-east-1.amazonaws.com/prod',
-  bff: 'http://victordobryi-bff-api-bffdev.us-east-1.elasticbeanstalk.com',
-  cart: 'http://victordobryi-bff-api-bffdev.us-east-1.elasticbeanstalk.com/cart',
+  bff: 'https://ipb1k38853.execute-api.us-east-1.amazonaws.com/',
+  cart: 'https://ipb1k38853.execute-api.us-east-1.amazonaws.com/cart',
 };
 
 export default API_PATHS;
